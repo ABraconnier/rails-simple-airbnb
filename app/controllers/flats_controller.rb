cde78edf07622fd5cea62dc21cd2d57ac44318e2
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-10-30 16:36:09 +0100

    index done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,21 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+end
